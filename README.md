fe8bd378282409334f5a7a5e3f095eb60a3acddb
Max Ranieri <otherranieri@gmail.com>
2019-01-25 20:57:35 -0500

    phaser boilerplate

---
+++ README.md
@@ -0,0 +1,2 @@
+# a game
+this is a game that we're making
---
+++ code/main.js
@@ -0,0 +1,32 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+var game = new Phaser.Game(config)
+var score = 0
+
+function preload () {
+  game.stage.backgroundColor="#4488AA"
+}
+
+function create (){
+
+}
+
+function update (){
+
+}
